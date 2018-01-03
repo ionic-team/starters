c61d12508137ab797f0087cfbe1349a1947cfbec
Daniel Imhoff <dwieeb@gmail.com>
2018-01-02 18:53:05 -0600

    wip: add home page

--- ionic-core-angular/base/src/app/app.component.ts
+++ ionic-core-angular/base/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title = 'app';
+
 }
