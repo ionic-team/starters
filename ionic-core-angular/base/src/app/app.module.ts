ae17144a4fef2260720ba7b3988e49762fdc3c63
Daniel Imhoff <dwieeb@gmail.com>
2018-01-02 20:01:59 -0600

    feat(ionic-core-angular): blank starter

--- ionic-core-angular/base/src/app/app.module.ts
+++ ionic-core-angular/base/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { IonicAngularModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { HomePage } from '../pages/home/home.page';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePage,
+  ],
+  entryComponents: [
+    HomePage,
   ],
   imports: [
     BrowserModule,
+    IonicAngularModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ ionic-core-angular/official/blank/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../pages/home/home.page';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent {
+
+  pageHome = HomePage;
+
+}
