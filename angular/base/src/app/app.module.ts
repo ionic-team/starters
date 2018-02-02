768a945c869cf9494cbee58b122f2c674f80d068
Daniel Imhoff <dwieeb@gmail.com>
2018-02-02 17:12:53 -0600

    feat(angular): start test setup

--- angular/base/src/app/app.module.ts
+++ angular/base/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicAngularModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
