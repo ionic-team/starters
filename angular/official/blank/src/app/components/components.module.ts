b03edd09b2928b40b0f4c3a3d2321f14e5d518ba
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2018-04-09 15:40:58 +0900

    fix(components) : add IonicModule

--- angular/official/blank/src/app/components/components.module.ts
+++ angular/official/blank/src/app/components/components.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   imports: [
     CommonModule,
+    IonicModule.forRoot(),
   ],
   declarations: [],
   entryComponents: [],
