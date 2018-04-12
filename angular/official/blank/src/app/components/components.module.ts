6a7f5e214956c6fee5761b61830105e8ca59869e
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2018-04-13 04:31:40 +0900

    fix(ngModule): add FormsModule (#81)

--- angular/official/blank/src/app/components/components.module.ts
+++ angular/official/blank/src/app/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 import { IonicModule } from "@ionic/angular";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     IonicModule.forRoot(),
   ],
   declarations: [],
--- angular/official/blank/src/app/pages/pages.module.ts
+++ angular/official/blank/src/app/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home/home.page';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     IonicModule.forRoot(),
   ],
   declarations: [
