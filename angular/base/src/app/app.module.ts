cc8e9e3da7ea68973d86f43968fd25c76c485fc2
Daniel Sogl <daniel@sogls.de>
2018-04-12 21:31:09 +0200

    feat(angular): add ionic native v5 (#84)

--- angular/base/src/app/app.module.ts
+++ angular/base/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot()],
+  providers: [StatusBar, SplashScreen],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
