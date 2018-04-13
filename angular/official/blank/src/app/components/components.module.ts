8643fced93218aee750727b59963c807418214fa
Daniel Imhoff <dwieeb@gmail.com>
2018-04-13 14:35:33 -0500

    style(angular): extra spaces in import

--- angular/official/blank/src/app/components/components.module.ts
+++ angular/official/blank/src/app/components/components.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from "@ionic/angular";
 
 @NgModule({
