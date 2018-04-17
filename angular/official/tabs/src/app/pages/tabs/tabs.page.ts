9ad3153392899515a40ea1d83822c425eee37bd9
Daniel Imhoff <dwieeb@gmail.com>
2018-04-17 17:45:04 -0500

    fix(angular/tabs): lint

--- angular/official/tabs/src/app/pages/tabs/tabs.page.ts
+++ angular/official/tabs/src/app/pages/tabs/tabs.page.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-  constructo(){
-    console.log('test here?')
+  constructor() {
+    console.log('test here?');
   }
 }
