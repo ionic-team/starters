c7aec75ddfe51e4745857f154f3ad69323768a39
jgw96 <jgw9617@gmail.com>
2018-05-07 16:18:35 -0500

    chore(): add service worker code to app.module

--- angular/official/tabs/src/app/app.module.ts
+++ angular/official/tabs/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, RouteReuseStrategy } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -16,7 +18,10 @@ import { AppComponent } from './app.component';
     IonicModule.forRoot(),
     RouterModule.forRoot([
       { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
-    ])
+    ]),
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
   providers: [StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
