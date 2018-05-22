de4b41246edbe73eff6f30db67dceb887247f103
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2018-05-22 23:44:30 +0900

    chore(blank): add form module (maybe just forgot) (#186)

--- angular/official/blank/src/app/pages/home/home.module.ts
+++ angular/official/blank/src/app/pages/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
@@ -8,6 +9,7 @@ import { HomePage } from './home.page';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     IonicModule,
     RouterModule.forChild([
       {
