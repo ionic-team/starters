b06e4788b8b870125fcabcf1005cbefa3d3c77d1
dwieeb <dwieeb@gmail.com>
2018-07-25 15:54:00 -0500

    fix(generate): flatten pages/ directory (#341)
    See https://github.com/ionic-team/ionic-cli/pull/3392 for more information.

--- angular/official/tabs/src/app/app-routing.module.ts
+++ angular/official/tabs/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
