05e547e316c7bcb66152b8408527de9b7fd3fa61
Daniel Imhoff <dwieeb@gmail.com>
2018-07-25 22:46:41 -0500

    fix(angular): match selector with those generated by `ionic g`
    addresses https://github.com/ionic-team/ionic-cli/issues/3389

--- angular/official/blank/src/app/app-routing.module.ts
+++ angular/official/blank/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- angular/official/sidemenu/src/app/list/list.page.ts
+++ angular/official/sidemenu/src/app/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-list',
+  selector: 'app-list',
   templateUrl: 'list.page.html',
   styleUrls: ['list.page.scss']
 })
