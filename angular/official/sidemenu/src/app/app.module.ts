ff2b3a9b87f57853f79f1c80661c6eee2dd4e89a
Daniel Imhoff <dwieeb@gmail.com>
2018-08-01 16:49:04 -0500

    remove import

--- angular/official/sidemenu/src/app/app.module.ts
+++ angular/official/sidemenu/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +15,7 @@ import { ComponentsModule } from './components/components.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule,
-    ComponentsModule
+    AppRoutingModule
   ],
   providers: [
     StatusBar,
