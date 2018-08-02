f8af85469fc5933854f7988cb941421e0aa6dedc
Daniel Imhoff <dwieeb@gmail.com>
2018-08-01 20:18:04 -0500

    fix(ionic1): fixes for new keyboard

--- ionic1/official/blank/www/js/app.js
+++ ionic1/official/blank/www/js/app.js
@@ -7,17 +7,19 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.Keyboard) {
+      window.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
       StatusBar.styleDefault();
     }
   });
