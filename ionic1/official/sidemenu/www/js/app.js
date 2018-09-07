71a40f8ede71a276430ef39590dd3e591592fbf6
Nicholas Hyatt <nicholaspaulhyatt@gmail.com>
2018-09-06 20:16:01 -0500

    fix(ionic1): rm window.Keyboard.hideKeyboardAccessoryBar call

--- ionic1/official/sidemenu/www/js/app.js
+++ ionic1/official/sidemenu/www/js/app.js
@@ -8,16 +8,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-
     if (window.StatusBar) {
       // Set the statusbar to use the default style, tweak this to
       // remove the status bar on iOS or change it to use white instead of dark colors.
