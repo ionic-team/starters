bc87392c4cc6e54d5811e70c68db596cba89ee82
榊原昌彦 <sakakibara@rdlabo.jp>
2018-09-29 01:58:55 +0900

    ref(polyfills): Follow Angular

--- angular/base/src/polyfills.ts
+++ angular/base/src/polyfills.ts
@@ -11,7 +11,7 @@
  * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
  * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
  *
- * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
+ * Learn more in https://angular.io/guide/browser-support
  */
 
 /***************************************************************************************************
@@ -28,11 +28,18 @@
 // import 'core-js/es6/math';
 // import 'core-js/es6/string';
 // import 'core-js/es6/date';
-// import 'core-js/es6/array';
 // import 'core-js/es6/regexp';
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
+
+/**
+ * If your app need to indexed by Google Search, your app require polyfills 'core-js/es6/array'
+ * Google bot use ES5.
+ * FYI: Googlebot uses a renderer following the similar spec to Chrome 41.
+ * https://developers.google.com/search/docs/guides/rendering
+ **/
+// import 'core-js/es6/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
