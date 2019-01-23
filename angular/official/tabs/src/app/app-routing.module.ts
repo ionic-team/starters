7a482dd0779c4e7a46293e3ae707a3379108b5a0
Mike Hartington <mhartington@users.noreply.github.com>
2019-01-23 13:35:17 -0500

    chore(angular): bump to Ionic 4.0.0 (#641)

--- angular/official/tabs/src/app/app-routing.module.ts
+++ angular/official/tabs/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
