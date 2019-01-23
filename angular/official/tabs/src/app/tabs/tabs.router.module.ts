1997065ba46f873e2d0c4825252b3693dc5f7365
mhartington <mikehartington@gmail.com>
2019-01-23 13:20:24 -0500

    chore(): update

--- angular/official/tabs/src/app/tabs/tabs.router.module.ts
+++ angular/official/tabs/src/app/tabs/tabs.router.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
@@ -50,7 +50,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.child(routes)
   ],
   exports: [RouterModule]
 })
