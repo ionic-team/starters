09daac28eb35ea7ad3b75aae73b78e81cb98d3c7
Josh Thomas <josh@ionic.io>
2019-04-18 09:53:54 -0500

    create react starters

---
+++ react/official/tabs/src/pages/Tab2.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+
+const Tab2: React.SFC = () => {
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab Two</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent />
+    </>
+  );
+};
+
+export default Tab2;
