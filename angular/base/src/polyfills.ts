19b5c773a89a53012b5112cf8071abc6fee58ef5
Liam DeBeasi <liamdebeasi@icloud.com>
2019-04-22 14:52:59 -0400

    add angular zone-flag with custom element flag disabled

--- angular/base/src/polyfills.ts
+++ angular/base/src/polyfills.ts
@@ -55,6 +55,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import './zone-flags.ts';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
