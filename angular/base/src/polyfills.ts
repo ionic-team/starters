128616cc6fc0107ca360fdf4411bd4d3cc5abfdc
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2019-04-29 09:16:47 -0400

    chore(): move zone-flags import up a line (#750)
    * chore(): move zone flags import
    
    * run linter

--- angular/base/src/polyfills.ts
+++ angular/base/src/polyfills.ts
@@ -52,10 +52,12 @@
  *
  */
 
+import './zone-flags.ts';
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import './zone-flags.ts';
+
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
