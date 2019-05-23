e4aeefda6e81391daa47f93599518f59ddd19acc
Matt Kremer <mkremer90@gmail.com>
2019-05-23 10:29:56 -0500

    Add constructors to starter pages

--- angular/official/sidemenu/src/app/home/home.page.ts
+++ angular/official/sidemenu/src/app/home/home.page.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  constructor(){
+    
+  }
+
 }
--- angular/official/tabs/src/app/tab2/tab2.page.ts
+++ angular/official/tabs/src/app/tab2/tab2.page.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(){
+    
+  }
+
+}
