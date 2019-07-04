9942a4adf356e447d7f270719141597ef74221dd
Justin Noel <justin.noel@calendee.com>
2019-07-04 07:12:44 -0500

    fix: add mocks and crypto to make tests pass

--- react/base/src/App.test.tsx
+++ react/base/src/App.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//@ts-ignore
+global.crypto = require('@trust/webcrypto');
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
