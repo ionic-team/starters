df5ab4384125ada7dec66e9b853e9e9ba614ef05
Cedric <cedric@dualstack.be>
2019-11-16 19:23:42 +0100

    add reference to theme.css styling

--- react/base/src/App.tsx
+++ react/base/src/App.tsx
@@ -16,6 +16,7 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 /* Theme variables */
+import './theme.css'
 import './theme/variables.css';
 
 const App: React.FunctionComponent = () => {
