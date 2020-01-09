513b270aea69368bb95eca495e3b3e0ac96c4b5c
Liam DeBeasi <liamdebeasi@icloud.com>
2020-01-09 12:02:01 -0500

    remove global scss from angular starters, add explore component

--- angular/official/tabs/src/app/tab1/tab1.module.ts
+++ angular/official/tabs/src/app/tab1/tab1.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
