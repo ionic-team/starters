5278c4018832253c1629a1a780363d627aa89626
Ely Lucas <ely@meta-tek.net>
2020-01-14 15:38:57 -0700

    chore(react): adding react list starter

---
+++ react/official/list/src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import MessageListItem from '../components/MessageListItem';
+import React, { useState } from 'react';
+import { Message, getMessages } from '../data/messages';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+  useIonViewWillEnter
+  } from '@ionic/react';
+import './Home.css';
+
+const Home: React.FC = () => {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useIonViewWillEnter(() => {
+    const msgs = getMessages();
+    setMessages(msgs);
+  });
+
+  return (
+    <IonPage id="home-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Inbox</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonRefresher slot="fixed">
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">
+              Inbox
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonList>
+          {messages.map(m => <MessageListItem key={m.id} message={m} />)}
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
