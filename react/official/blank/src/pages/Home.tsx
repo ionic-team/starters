fec6673f097f80b6cb998b51689dee776de209b1
Liam DeBeasi <liamdebeasi@icloud.com>
2020-01-28 14:50:10 -0500

    Add dark mode base, update blank

--- react/official/blank/src/pages/Home.tsx
+++ react/official/blank/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
+import './Home.css';
 
 const Home: React.FC = () => {
   return (
