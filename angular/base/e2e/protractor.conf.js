2ff0d7ea9aeeadbf535199e15a67a9aa4512165e
Daniel Sogl <daniel.sogl@cellent.de>
2020-02-12 09:44:07 +0100

    feat(angular): update base project to angular cli v9

--- angular/base/e2e/protractor.conf.js
+++ angular/base/e2e/protractor.conf.js
@@ -1,15 +1,19 @@
+// @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
+/**
+ * @type { import("protractor").Config }
+ */
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
