d0e2e9e6b9799fad352dc5111bcb21cb31c9e1c5
Liam DeBeasi <liamdebeasi@icloud.com>
2020-08-27 15:08:23 -0400

    add vue blank starter

---
+++ vue/official/blank/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+
+export default {
+  setup() {
+    return { IonApp, IonRouterOutlet }
+  }
+};
+</script>
---
+++ vue/official/blank/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Blank</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-input @ioninput="onChange($event)"></ion-input>
+    
+      <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonInput, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import './Home.scss';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonInput,
+    IonToolbar
+  },
+  setup() {
+    return {
+      onChange: (ev: any) => {
+        console.log('on change',ev)
+      }
+    }
+  }
+});
+</script>
