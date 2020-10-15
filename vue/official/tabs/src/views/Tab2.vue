025e96da063e2e8fd5abe7725ae89a910e75fb94
Liam DeBeasi <liamdebeasi@icloud.com>
2020-10-15 16:39:29 +0000

    add ts lang

--- vue/official/tabs/src/views/Tab2.vue
+++ vue/official/tabs/src/views/Tab2.vue
@@ -17,7 +17,7 @@
   </ion-page>
 </template>
 
-<script>
+<script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
