c51d50c681d508134acb5c3cf7f5b30b1842fe97
Ely Lucas <ely@meta-tek.net>
2020-11-30 08:45:52 -0700

    feat(react): update react deps to use react 17, cra 4.0.1, and TS 4.1.2

--- react/official/sidemenu/src/pages/Page.tsx
+++ react/official/sidemenu/src/pages/Page.tsx
@@ -1,5 +1,4 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
