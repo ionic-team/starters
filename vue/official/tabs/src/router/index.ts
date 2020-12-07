77f756c925f46db28214af5f38da196292feae44
Amit Gurbani <amit1994.gurbani@gmail.com>
2020-12-07 22:04:04 +0530

    Update index.ts
    redirect: 'tab1' currently redirects to /tab1 (which does not exist), rather it should be /tabs/tab1

--- vue/official/tabs/src/router/index.ts
+++ vue/official/tabs/src/router/index.ts
@@ -13,7 +13,7 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: 'tab1'
+        redirect: '/tabs/tab1'
       },
       {
         path: 'tab1',
