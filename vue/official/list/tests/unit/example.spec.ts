e1823bd7861789e105d01bf13c280242723e2817
Liam DeBeasi <liamdebeasi@icloud.com>
2021-01-05 15:53:57 +0000

    typo

--- vue/official/list/tests/unit/example.spec.ts
+++ vue/official/list/tests/unit/example.spec.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import Home from '@/views/Home.vue'
 
 describe('Home.vue', () => {
-  it('should render home view', () => {
+  it('renders home view', () => {
     const wrapper = mount(Home)
     expect(wrapper.text()).toMatch('Inbox')
   })
