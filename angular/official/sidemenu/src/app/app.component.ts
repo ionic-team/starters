83ff9dc261be4ed0c6e45706d0b41c79876935e2
Mike Hartington <mikehartington@gmail.com>
2021-02-01 15:45:19 -0500

    chore(angular): update active state in sidemenu Closes #1194

--- angular/official/sidemenu/src/app/app.component.ts
+++ angular/official/sidemenu/src/app/app.component.ts
@@ -1,54 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  public selectedIndex = 0;
+export class AppComponent {
   public appPages = [
-    {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail',
-    },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane',
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart',
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive',
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash',
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning',
-    },
+    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
   constructor() {}
-
-  ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(
-        (page) => page.title.toLowerCase() === path.toLowerCase()
-      );
-    }
-  }
 }
