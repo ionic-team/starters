f25b1b9d5c0e295c39e4decc3da671241baae1fe
Jacob McKay <metallimoob@gmail.com>
2021-02-07 19:59:25 -0600

    Fix `ionic start sidemenu`
    The ionic sidemenu starter project fails during a production build because selectIndex is used in the UI but isn't defined on the component class.

--- angular/official/sidemenu/src/app/app.component.ts
+++ angular/official/sidemenu/src/app/app.component.ts
@@ -13,6 +13,9 @@ export class AppComponent {
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
+  
+  public selectedIndex: number;
+  
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
