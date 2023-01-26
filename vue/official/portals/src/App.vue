36081576c4fea122cc37871783e305bd7beea5b6
Josh Thomas <jthoms1@gmail.com>
2023-01-25 19:10:47 -0600

    begin portals starters

---
+++ vue/official/portals/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { getInitialContext } from '@ionic/portals';
+
+const initialContext = getInitialContext<any>();
+</script>
