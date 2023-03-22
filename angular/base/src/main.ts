a74f2207c64222803e9637dc3c122f46c04856f0
Mike Hartington <mikehartington@gmail.com>
2023-03-22 14:42:06 -0400

    fix(): update import paths

--- angular/base/src/main.ts
+++ angular/base/src/main.ts
@@ -3,7 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { routes } from './app/routes';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
--- angular/official/tabs/src/app/app.routes.ts
+++ angular/official/tabs/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs-routes').then((m) => m.routes),
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
 ];
