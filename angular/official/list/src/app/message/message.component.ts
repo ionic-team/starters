6420a02fc021e8fe15411c7b8ea71f91a94d8cae
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-17 16:29:44 -0400

    Update message.component.ts

--- angular/official/list/src/app/message/message.component.ts
+++ angular/official/list/src/app/message/message.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { Message } from '../services/data.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessageComponent {
+  private platform = inject(Platform);
   @Input() message?: Message;
-
   isIos() {
-    const win = window as any;
-    return win && win.Ionic && win.Ionic.mode === 'ios';
+    return this.platform.is('ios')
   }
 }
