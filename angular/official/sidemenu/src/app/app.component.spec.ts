c0082bd3e9d43813fcacdf485427b2dd85a3616b
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-04-17 20:40:27 +0000

    fix test

--- angular/official/sidemenu/src/app/app.component.spec.ts
+++ angular/official/sidemenu/src/app/app.component.spec.ts
@@ -39,8 +39,8 @@ describe('AppComponent', () => {
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
     expect(menuItems.length).toEqual(12);
-    expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/folder/Inbox');
-    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/folder/Outbox');
+    expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual('/folder/inbox');
+    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual('/folder/outbox');
   });
 
 });
