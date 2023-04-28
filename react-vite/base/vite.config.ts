49abd482ce50cb01154e199d7a341d9b111a94da
Sean Perkins <sean@ionic.io>
2023-04-28 12:52:14 -0400

    fix(react): add support for ionic supported browser versions

--- react-vite/base/vite.config.ts
+++ react-vite/base/vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy()
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
