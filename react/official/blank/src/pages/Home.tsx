53f6e1c8f10521636860b49205e62bf56b26abac
Maria Hutt <thetaPC@users.noreply.github.com>
2023-05-16 14:21:53 -0700

    chore(react): include eslint (#1779)

--- react/official/blank/src/pages/Home.tsx
+++ react/official/blank/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
--- react/official/tabs/src/App.tsx
+++ react/official/tabs/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
