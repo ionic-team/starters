644e19b87a51f09ad0bab6800a7868c65feec515
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-06-29 10:24:03 -0400

    chore: make selector consistent

--- react-vite/official/blank/src/components/ExploreContainer.tsx
+++ react-vite/official/blank/src/components/ExploreContainer.tsx
@@ -4,7 +4,7 @@ interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
-    <div className="container">
+    <div id="container">
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
