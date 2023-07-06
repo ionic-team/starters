f8535933342c5d66a5c387d5cf46910d87bae58a
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-07-06 11:23:54 -0400

    chore(react, vue): update major depenencies (#1787)

--- react/official/blank/src/components/ExploreContainer.tsx
+++ react/official/blank/src/components/ExploreContainer.tsx
@@ -3,7 +3,7 @@ import './ExploreContainer.css';
 
 const ExploreContainer: React.FC = () => {
   return (
-    <div className="container">
+    <div id="container">
       <strong>Ready to create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
