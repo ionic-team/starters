a68dd52c9ae5768fb08bbf8d25d517dae07ec738
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-08-14 10:41:27 -0500

    test: angular, react, and vue apps are tested on CI (#1795)

--- angular-standalone/official/tabs/src/app/tab1/tab1.page.spec.ts
+++ angular-standalone/official/tabs/src/app/tab1/tab1.page.spec.ts
@@ -7,10 +7,6 @@ describe('Tab1Page', () => {
   let fixture: ComponentFixture<Tab1Page>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Tab1Page],
-    }).compileComponents();
-
     fixture = TestBed.createComponent(Tab1Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
