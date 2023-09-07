1b7f26e842173536d9cdacf5f377f41d791691d8
Maria Hutt <maria@ionic.io>
2023-09-07 14:17:29 -0700

    feat(angular): remove rest of IonicModule

--- angular-standalone/official/tabs/src/app/app.component.ts
+++ angular-standalone/official/tabs/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [CommonModule, IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
