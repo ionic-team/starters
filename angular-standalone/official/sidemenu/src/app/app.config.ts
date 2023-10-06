efd9009d14b3dfa21b9445b4ff5ab35fe4bbe64c
榊原昌彦 <sakakibara@rdlabo.jp>
2023-10-06 16:52:19 +0900

    feat(angular-standalone): split app.config.ts and main.ts

---
+++ angular-standalone/official/sidemenu/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    importProvidersFrom(IonicModule.forRoot({})),
+    provideRouter(routes),
+  ],
+};
