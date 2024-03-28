50493cef5602b12dbf528f9d17fb8e73415d9128
Ionitron <hi@ionicframework.com>
2024-03-28 09:33:03 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import legacy from '@vitejs/plugin-legacy'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  }
+})
