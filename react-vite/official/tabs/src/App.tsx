cf9bd1cf3e84bbaf2c33b7a6ac4bc08bcfc3415a
Brandy Carney <brandyscarney@users.noreply.github.com>
2024-04-17 13:10:18 -0400

    chore(ionic): update to v8 (#1839)
    ---------
    
    Co-authored-by: Shawn Taylor <shawn@ionic.io>
    Co-authored-by: Sean Perkins <13732623+sean-perkins@users.noreply.github.com>

--- react-vite/official/tabs/src/App.tsx
+++ react-vite/official/tabs/src/App.tsx
@@ -31,6 +31,17 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css';
+
 /* Theme variables */
 import './theme/variables.css';
 
--- vue-vite/base/src/main.ts
+++ vue-vite/base/src/main.ts
@@ -20,13 +20,24 @@ import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
 
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* @import '@ionic/vue/css/palettes/dark.always.css'; */
+/* @import '@ionic/vue/css/palettes/dark.class.css'; */
+import '@ionic/vue/css/palettes/dark.system.css';
+
 /* Theme variables */
 import './theme/variables.css';
 
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
-  
+
 router.isReady().then(() => {
   app.mount('#app');
 });
