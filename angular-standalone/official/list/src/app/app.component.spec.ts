9b4e94b497d1b2831eaa7b9b60572fe0be4dffa9
Sean Perkins <sean@ionic.io>
2024-04-24 16:08:49 -0400

    fix: update list and side menu templates

--- angular-standalone/official/list/src/app/app.component.spec.ts
+++ angular-standalone/official/list/src/app/app.component.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 
   beforeEach(async () => {
-    TestBed.overrideComponent(AppComponent, {
-      add: {
-        imports: [RouterModule]
-      }
-    });
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
   });
 
   it('should create the app', () => {
