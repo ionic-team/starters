0585aca7f75ea36440bddb37a5638c26c861a360
Sean Perkins <13732623+sean-perkins@users.noreply.github.com>
2024-04-25 13:53:11 -0400

    refactor(angular): migrate deprecated RouterTestingModule (#1843)
    * refactor(angular): migrate deprecated RouterTestingModule
    
    RouterTestingModule was deprecated in v17.3.0 of Angular and marked as deprecate here: https://github.com/angular/angular/pull/54466
    
    * fix: updates for standalone apps
    
    * fix: update list and side menu templates
    
    * fix: sidemenu folder page test

--- angular-standalone/base/src/app/app.component.spec.ts
+++ angular-standalone/base/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  it('should create the app', () => {
-    TestBed.overrideComponent(AppComponent, {
-      add: {
-        imports: [RouterTestingModule]
-      }
-    });
+  it('should create the app', async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
+    
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
--- angular-standalone/official/sidemenu/src/app/app.component.spec.ts
+++ angular-standalone/official/sidemenu/src/app/app.component.spec.ts
@@ -1,15 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
-    TestBed.overrideComponent(AppComponent, {
-      add: {
-        imports: [RouterTestingModule]
-      }
-    });
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+      providers: [provideRouter([])]
+    }).compileComponents();
   });
 
   it('should create the app', () => {
