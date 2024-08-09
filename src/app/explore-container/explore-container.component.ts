1529f43a837cb498fe9e9c7063f3df2093e0fa35
Ionitron <hi@ionicframework.com>
2024-08-09 09:33:04 +0000

    Initial commit

---
+++ src/app/explore-container/explore-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-container',
+  templateUrl: './explore-container.component.html',
+  styleUrls: ['./explore-container.component.scss'],
+})
+export class ExploreContainerComponent {
+
+  @Input() name?: string;
+
+}
