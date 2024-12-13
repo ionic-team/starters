ca7f4b8be01e6fa39d221f90bf150df58b25c71c
Brandy Carney <6577830+brandyscarney@users.noreply.github.com>
2024-12-13 12:45:51 -0500

    fix(angular): fix invalid route redirect

--- angular/official/sidemenu/src/app/app-routing.module.ts
+++ angular/official/sidemenu/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'folder/inbox',
     pathMatch: 'full'
   },
   {
