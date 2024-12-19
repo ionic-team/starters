03444edbd4ce1bbf05137ea308daa91e0234a4b1
Ionitron <hi@ionicframework.com>
2024-12-19 09:34:43 +0000

    Initial commit

---
+++ src/pages/Page.tsx
@@ -0,0 +1,33 @@
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useParams } from 'react-router';
+import ExploreContainer from '../components/ExploreContainer';
+import './Page.css';
+
+const Page: React.FC = () => {
+
+  const { name } = useParams<{ name: string; }>();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>{name}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{name}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name={name} />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Page;
