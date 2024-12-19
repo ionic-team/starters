7c829d7616b44b8bc5c8b0969eb516a122d98ef0
Ionitron <hi@ionicframework.com>
2024-12-19 09:34:44 +0000

    Initial commit

---
+++ src/app/view-message/view-message.page.ts
@@ -0,0 +1,30 @@
+
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonicModule, Platform } from '@ionic/angular';
+import { DataService, Message } from '../services/data.service';
+
+@Component({
+  selector: 'app-view-message',
+  templateUrl: './view-message.page.html',
+  styleUrls: ['./view-message.page.scss'],
+  standalone: false,
+})
+export class ViewMessagePage implements OnInit {
+  public message!: Message;
+  private data = inject(DataService);
+  private activatedRoute = inject(ActivatedRoute);
+  private platform = inject(Platform);
+
+  constructor() {}
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    this.message = this.data.getMessageById(parseInt(id, 10));
+  }
+
+  getBackButtonText() {
+    const isIos = this.platform.is('ios')
+    return isIos ? 'Inbox' : '';
+  }
+}
