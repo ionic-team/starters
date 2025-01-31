c3eff090101d759284ff1ec960aa5d8fed53685b
Ionitron <hi@ionicframework.com>
2025-01-31 09:32:57 +0000

    Initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+import { HomePage } from './home.page';
+import { HomePageRoutingModule } from './home-routing.module';
+import { MessageComponentModule } from '../message/message.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    MessageComponentModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
