1caac13a7b8688b06b2c1f927d5f92e5741fb18b
Ionitron <hi@ionicframework.com>
2025-01-31 09:32:52 +0000

    Initial commit

---
+++ src/app/folder/folder.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { FolderPageRoutingModule } from './folder-routing.module';
+
+import { FolderPage } from './folder.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    FolderPageRoutingModule
+  ],
+  declarations: [FolderPage]
+})
+export class FolderPageModule {}
