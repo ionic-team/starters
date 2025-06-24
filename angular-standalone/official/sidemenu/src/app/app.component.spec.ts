f23132d675415e7fc83741431f11a5583f1343fd
Maria Hutt <thetaPC@users.noreply.github.com>
2025-06-23 17:24:32 -0700

    test(angular): use href instead of ng-reflect-*

--- angular-standalone/official/sidemenu/src/app/app.component.spec.ts
+++ angular-standalone/official/sidemenu/src/app/app.component.spec.ts
@@ -33,10 +33,10 @@ describe('AppComponent', () => {
     const app = fixture.nativeElement;
     const menuItems = app.querySelectorAll('ion-item');
     expect(menuItems.length).toEqual(12);
-    expect(menuItems[0].getAttribute('ng-reflect-router-link')).toEqual(
+    expect(menuItems[0].getAttribute('href')).toEqual(
       '/folder/inbox'
     );
-    expect(menuItems[1].getAttribute('ng-reflect-router-link')).toEqual(
+    expect(menuItems[1].getAttribute('href')).toEqual(
       '/folder/outbox'
     );
   });
