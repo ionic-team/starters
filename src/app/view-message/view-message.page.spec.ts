3d99c0b79dde7f7710f54287881e61307d4da1f0
Ionitron <hi@ionicframework.com>
2025-06-29 09:34:58 +0000

    Initial commit

---
+++ src/app/view-message/view-message.page.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+import { ViewMessagePageRoutingModule } from './view-message-routing.module';
+import { ViewMessagePage } from './view-message.page';
+
+describe('ViewMessagePage', () => {
+  let component: ViewMessagePage;
+  let fixture: ComponentFixture<ViewMessagePage>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [ViewMessagePage],
+      imports: [IonicModule.forRoot(), ViewMessagePageRoutingModule, RouterModule.forRoot([])]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ViewMessagePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
