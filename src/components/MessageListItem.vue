d9c713fa7124e520417aa9e4f109beb7c9e1ca7d
Ionitron <hi@ionicframework.com>
2025-06-29 09:34:50 +0000

    Initial commit

---
+++ src/components/MessageListItem.vue
@@ -0,0 +1,99 @@
+<template>
+  <ion-item v-if="message" :routerLink="'/message/' + message.id" :detail="false" class="list-item">
+    <div slot="start" :class="!message.read ? 'dot dot-unread' : 'dot'"></div>
+    <ion-label class="ion-text-wrap">
+      <h2>
+        {{ message.fromName }}
+        <span class="date">
+          <ion-note>{{ message.date }}</ion-note>
+          <ion-icon aria-hidden="true" :icon="chevronForward" size="small" v-if="isIos()"></ion-icon>
+        </span>
+      </h2>
+      <h3>{{ message.subject }}</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </ion-label>
+  </ion-item>
+</template>
+
+<script setup lang="ts">
+import { IonIcon, IonItem, IonLabel, IonNote } from '@ionic/vue';
+import { chevronForward } from 'ionicons/icons';
+
+defineProps({
+  message: Object,
+});
+
+const isIos = () => {
+  const win = window as any;
+  return win && win.Ionic && win.Ionic.mode === 'ios';
+};
+</script>
+
+<style scoped>
+.list-item {
+  --padding-start: 0;
+  --inner-padding-end: 0;
+}
+
+.list-item ion-label {
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+
+.list-item h2 {
+  font-weight: 600;
+  margin: 0;
+  
+  /**
+   * With larger font scales
+   * the date/time should wrap to the next
+   * line. However, there should be
+   * space between the name and the date/time
+   * if they can appear on the same line.
+   */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.list-item p {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 95%;
+}
+
+.list-item .date {
+  align-items: center;
+  display: flex;
+}
+
+.list-item ion-icon {
+  color: #c9c9ca;
+}
+
+.list-item ion-note {
+  font-size: 0.9375rem;
+  margin-right: 8px;
+  font-weight: normal;
+}
+
+.list-item ion-note.md {
+  margin-right: 14px;
+}
+
+.list-item .dot {
+  display: block;
+  height: 12px;
+  width: 12px;
+  border-radius: 50%;
+  align-self: start;
+  margin: 16px 10px 16px 16px;
+}
+
+.list-item .dot-unread {
+  background: var(--ion-color-primary);
+}
+</style>
