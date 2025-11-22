eec878fd3ba81d738289be3cff3f7fa196747e0b
Ionitron <hi@ionicframework.com>
2025-11-22 09:34:03 +0000

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { RefresherCustomEvent } from '@ionic/angular';
+import { MessageComponent } from '../message/message.component';
+
+import { DataService, Message } from '../services/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  private data = inject(DataService);
+  constructor() {}
+
+  refresh(ev: any) {
+    setTimeout(() => {
+      (ev as RefresherCustomEvent).detail.complete();
+    }, 3000);
+  }
+
+  getMessages(): Message[] {
+    return this.data.getMessages();
+  }
+}
