2e41f9f08c447414634f3b157592ec539e4594ef
Ionitron <hi@ionicframework.com>
2026-09-03 09:38:08 +0000

    Initial commit

---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import Tab1Page from '@/views/Tab1Page.vue'
+import { describe, expect, test } from 'vitest'
+
+describe('Tab1Page.vue', () => {
+  test('renders tab 1 Tab1Page', () => {
+    const wrapper = mount(Tab1Page)
+    expect(wrapper.text()).toMatch('Tab 1 page')
+  })
+})
