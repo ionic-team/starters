758436b1ee77766a3496639f5fd1aeed7ad93ef3
Ionitron <hi@ionicframework.com>
2026-09-03 09:37:01 +0000

    Initial commit

---
+++ src/app/services/data.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { DataService } from './data.service';
+
+describe('DataService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: DataService = TestBed.inject(DataService);
+    expect(service).toBeTruthy();
+  });
+});
