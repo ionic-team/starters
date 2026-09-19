abd2f944816e7586c254e706b730560e2af5039f
Ionitron <hi@ionicframework.com>
2026-09-19 09:34:24 +0000

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { RefresherCustomEvent } from '@ionic/angular/lazy';
+
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class HomePage {
+  private data = inject(DataService);
+  protected readonly messages = this.data.getMessages();
+
+  refresh(ev: any) {
+    setTimeout(() => {
+      (ev as RefresherCustomEvent).detail.complete();
+    }, 3000);
+  }
+}
---
+++ src/app/view-message/view-message.page.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular/lazy';
+import { RouterModule } from '@angular/router';
+
+import { ViewMessagePageRoutingModule } from './view-message-routing.module';
+import { ViewMessagePage } from './view-message.page';
+
+describe('ViewMessagePage', () => {
+  let component: ViewMessagePage;
+  let fixture: ComponentFixture<ViewMessagePage>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [ViewMessagePage],
+      imports: [IonicModule.forRoot(), ViewMessagePageRoutingModule, RouterModule.forRoot([])]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ViewMessagePage);
+    component = fixture.componentInstance;
+    fixture.componentRef.setInput('id', '0');
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
