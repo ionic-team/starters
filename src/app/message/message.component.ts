269728fc9cb1aee44e41a7bc5271dc95efe5210a
Ionitron <hi@ionicframework.com>
2026-09-26 09:35:58 +0000

    Initial commit

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { Platform } from '@ionic/angular/lazy';
+import { Message } from '../services/data.service';
+
+@Component({
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss'],
+  standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MessageComponent {
+  private platform = inject(Platform);
+  readonly message = input<Message>();
+  readonly isIos = this.platform.is('ios');
+}
