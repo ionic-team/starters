c791208564eca3027bfa5bc3ba9c44869fb783c2
Ionitron <hi@ionicframework.com>
2026-09-26 09:36:59 +0000

    Initial commit

---
+++ src/views/ViewMessagePage.vue
@@ -0,0 +1,113 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button :text="getBackButtonText()" default-href="/"></ion-back-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true" v-if="message">
+      <ion-item>
+        <ion-icon aria-hidden="true" :icon="personCircle" color="primary"></ion-icon>
+        <ion-label class="ion-text-wrap">
+          <h2>
+            {{ message.fromName }}
+            <span class="date">
+              <ion-note>{{ message.date }}</ion-note>
+            </span>
+          </h2>
+          <h3>To: <ion-note>Me</ion-note></h3>
+        </ion-label>
+      </ion-item>
+
+      <div class="ion-padding">
+        <h1>{{ message.subject }}</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonNote,
+  IonPage,
+  IonToolbar,
+} from '@ionic/vue';
+import { personCircle } from 'ionicons/icons';
+import { getMessage } from '../data/messages';
+
+const getBackButtonText = () => {
+  const win = window as any;
+  const mode = win && win.Ionic && win.Ionic.mode;
+  return mode === 'ios' ? 'Inbox' : '';
+};
+
+const route = useRoute();
+const message = getMessage(parseInt(route.params.id as string, 10));
+</script>
+
+<style scoped>
+ion-item {
+  --inner-padding-end: 0;
+  --background: transparent;
+}
+
+ion-label {
+  margin-top: 12px;
+  margin-bottom: 12px;
+}
+
+ion-item h2 {
+  font-weight: 600;
+  
+  /**
+   * With larger font scales
+   * the date/time should wrap to the next
+   * line. However, there should be
+   * space between the name and the date/time
+   * if they can appear on the same line.
+   */
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+ion-item .date {
+  align-items: center;
+  display: flex;
+}
+
+ion-item ion-icon {
+  font-size: 42px;
+  margin-right: 8px;
+}
+
+ion-item ion-note {
+  font-size: 0.9375rem;
+  margin-right: 12px;
+  font-weight: normal;
+}
+
+h1 {
+  margin: 0;
+  font-weight: bold;
+  font-size: 1.4rem;
+}
+
+p {
+  line-height: 1.4;
+}
+</style>
