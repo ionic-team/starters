92519b773d19d8896aafb88299fae85dc2507544
Ionitron <hi@ionicframework.com>
2026-09-26 09:35:48 +0000

    Initial commit

---
+++ src/app/folder/folder.page.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular/lazy';
+
+import { FolderPage } from './folder.page';
+
+describe('FolderPage', () => {
+  let component: FolderPage;
+  let fixture: ComponentFixture<FolderPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FolderPage],
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FolderPage);
+    component = fixture.componentInstance;
+    fixture.componentRef.setInput('folder', 'Inbox');
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
